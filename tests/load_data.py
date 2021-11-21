5a99b781a5208925dfa67e1a417045d5eb190b2a
datagutten <datagutten@datagutten.net>
2021-11-21 20:33:36 +0100

    Log level debug

--- tests/load_data.py
+++ tests/load_data.py
@@ -9,7 +9,7 @@ process = slapd.Slapd(
     root_cn='admin',
     root_pw='test',
     port=389,
-    log_level=logging.INFO,
+    log_level=logging.DEBUG,
     suffix='DC=example,DC=com',
     # debug=True,
 )
